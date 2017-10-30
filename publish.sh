0480d22955cc32cbb904733c1aaea2374feffb33
Ben Weissmann <benweissmann@gmail.com>
2017-10-30 00:34:36 -0400

    Publish with mime type text/plain

--- publish.sh
+++ publish.sh
@@ -4,7 +4,7 @@ SHA=$(shasum -a 256 index.sh | cut -d ' ' -f1)
 gsed -i 's/The sha256 checksum is `.*`/The sha256 checksum is `'$SHA'`/' README.md
 
 aws s3 cp ./index.sh s3://getmic.ro/index.sh \
-  --content-type 'text/html' \
+  --content-type 'text/plain' \
   --cache-control 'max-age=60' \
   --acl public-read
 
