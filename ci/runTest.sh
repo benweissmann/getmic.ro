14e3bd50e78f3c72ac20204da63368a09892f42e
Ben Weissmann <benweissmann@gmail.com>
2020-05-17 15:53:52 -0400

    Fix tests and add better error messages

--- ci/runTest.sh
+++ ci/runTest.sh
@@ -5,16 +5,19 @@ set -eu
 shell=${1:-bash}
 
 cat index.sh | "$shell"
-./micro -version | grep 'Version:'
+(./micro -version | grep 'Version:') || \
+  (echo 'Fail: micro installation test' && exit 1)
 
 unset GETMICRO_PLATFORM
-export OSTYPE='xxx'
+export PATH="./ci/fixtures:$PATH"
 
-cat index.sh | "$shell" | grep 'COULD NOT DETECT PLATFORM'
+(cat index.sh | "$shell" | grep 'COULD NOT DETECT PLATFORM') || \
+  (echo 'Fail: unrecognized platform test' && exit 1)
 
 export GETMICRO_PLATFORM=linux32
-cat index.sh | "$shell" | grep 'Detected platform: linux32'
+(cat index.sh | "$shell" | grep 'Detected platform: linux32') || \
+  (echo 'Fail: linux32 override test' && exit 1)
 
-export OSTYPE='darwin'
 export GETMICRO_PLATFORM=linux64
-cat index.sh | "$shell" | grep 'Detected platform: linux64'
+(cat index.sh | "$shell" | grep 'Detected platform: linux64') || \
+  (echo 'Fail: linux64 override test' && exit 1)
