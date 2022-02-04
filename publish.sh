31fe8ef4f5848e10970b227e7ae676aced491c53
Jack <wowzeryest@gmail.com>
2022-02-04 13:25:51 -0500

    Revamped index.sh to my liking

--- publish.sh
+++ publish.sh
@@ -2,8 +2,34 @@
 
 set -euo pipefail
 
+cd "$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+cmdexists() {
+  command -v "$1" >/dev/null 2>&1
+}
+
+if ! cmdexists gsed ; then
+  if cmdexists sed && sed --version | grep -q '(GNU sed)' ; then
+    # gsed doesnt exist on many distros; instead, sed 
+    gsed() {
+      sed "$@"
+    }
+  else
+    echo 'FATAL ERROR: GNU sed is needed by this script but is not installed. Please install gsed' 1>&2
+    exit 1
+  fi
+fi
+
+if cmdexists apt-get && cmdexists sudo ; then
+  # according to order of operations, this is ! ( cmdexists aws && cmdexists git )
+  if ! cmdexists aws && cmdexists git ; then
+    echo 'Need to install aws and git in order to proceed' 1>&2
+    sudo apt-get install -y awscli git
+  fi
+fi
+
 SHA=$(shasum -a 256 index.sh | cut -d ' ' -f1)
-OLDSHA=$(rg -o 'The sha256 checksum is `([a-f0-9]+)`' -r '$1' < README.md)
+OLDSHA=$(grep -oe '<!--shasum=.*-->' README.md | grep -Eoe '[a-f0-9]{64}')
 gsed -i "s/$OLDSHA/$SHA/" README.md
 gsed -i "s/$OLDSHA/$SHA/" README.fr.md
 
